477b95e00f7d9e4a27d52647f69cc4723a0174f9
Pankaj Kumar <97273195+gtpankajjha@users.noreply.github.com>
2026-08-17 14:51:58 +0530

    Working section of Candidate and Employer

---
+++ frontend/config.ts
@@ -0,0 +1,10 @@
+// Firebase project configuration
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBQ3O6_pT4HuWGv6ORRlxCE5Gd-Wh1xnck",
+  authDomain: "project-ebefd0ca-99a2-45c6-968.firebaseapp.com",
+  projectId: "project-ebefd0ca-99a2-45c6-968",
+  storageBucket: "project-ebefd0ca-99a2-45c6-968.firebasestorage.app",
+  messagingSenderId: "1079770572829",
+  appId: "1:1079770572829:web:5d3f367296d1f072323ef4",
+  measurementId: "G-EDB47RS5HG"
+};
