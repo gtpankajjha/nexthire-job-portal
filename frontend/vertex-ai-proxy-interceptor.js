df9b0c542aaff770f5f251b8b03f2c6c7e3a436f
Pankaj Kumar <97273195+gtpankajjha@users.noreply.github.com>
2026-08-18 11:18:40 +0530

    Updates flows of the app and ui corrections

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'Zrvlo3rXrecT8PawzW4BqdsVbrvUR3AJ',
+            'X-App-Proxy': '1PghzTgWF-pS9NtlPS2Ie21cBHajFfYa',
           },
           body: JSON.stringify(requestDetails),
         };
